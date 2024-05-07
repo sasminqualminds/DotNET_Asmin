721d75da115988873126bf4377aee21eaa4e4b43
sasminqualminds <sasmin@qualminds.com>
2024-05-07 19:24:06 +0530

    HttpMethodsWebApi1.1

---
+++ Day14 (05-05-2024)/HttpMethodsWebApi1.1/Controllers/EmployeesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private static List<Employee> employees = new List<Employee>();
+        Employee employee = new Employee();
+
+        static EmployeesController()
+        {
+            employees.Add(new Employee() { Id = 1, Name = "Jack", Age = 10 });
+            employees.Add(new Employee() { Id = 2, Name = "Jim", Age = 20 });
+            employees.Add(new Employee() { Id = 3, Name = "Alex", Age = 30 });
+            employees.Add(new Employee() { Id = 4, Name = "Jam", Age = 40 });
+        }
+
+        // Reading the data using GET request
+        [HttpGet(Name = "GetEmployeeData")]
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+        // Reading the data by Id using GET request
+        [HttpGet("{id}")]
+        public Employee GetEmployeeById(int id)
+        {
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == id)
+                {
+                    return emp;
+                }
+            }
+            return null;
+        }
+
+        // Adding the data using POST request
+        [HttpPost]
+        public ContentResult AddEmployee(Employee employee)
+        {
+            // Checking if an employee with the same ID already exists
+            foreach(Employee emp in employees)
+            {
+                if (emp.Id==employee.Id)
+                {
+                    return Content("Employee with the same ID already exists.");
+                }
+            }           
+            // Adding the new employee to the list
+            employees.Add(employee);
+            return Content("Employee Added");
+        }
+
+        // Deleting the data using DELETE request
+        [HttpDelete("{id}")]
+        public ContentResult DeleteEmployee(int id)
+        {
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == id)
+                {
+                    employees.Remove(emp);
+                    return Content("removed Successfully");
+                }
+            }
+            return Content($"Employee with id {id} not found");
+        }
+
+        // Updating the data using PUT request
+        [HttpPut("{id}")]
+        public ContentResult UpdateEmployee(int id, Employee employee)
+        {
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == id)
+                {
+                    emp.Name=employee.Name;
+                    emp.Age=employee.Age;
+                    emp.Salary = employee.Salary;
+                    return Content("Updated successfully");
+                }
+            }
+            return Content($"Employee with id {id} not found");
+        }
+    }
+}
