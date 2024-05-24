1be7d8864a78ab374ff9efe24d57e053e2788665
sasminqualminds <160615302+sasminqualminds@users.noreply.github.com>
2024-05-24 11:32:30 +0530

    Delete Day17 (12-05-2024) directory

--- Day17 (12-05-2024)/AsyncCPDetails/Model/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AsyncCPDetails.Model
-{
-    public class Customer
-    {
-        public int Id { get; set; }
-        public  string CustomerName { get; set; }
-        public  string Phone { get; set; }
-    }
-}
