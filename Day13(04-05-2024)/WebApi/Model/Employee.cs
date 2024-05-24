efd1922e3af5a74b25941b6fd1ebb6850315878f
sasminqualminds <sasmin@qualminds.com>
2024-05-24 11:35:05 +0530

    MVC

---
+++ Day13(04-05-2024)/WebApi/Model/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Controllers
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
